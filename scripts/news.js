a71cec1d909859740c92d856c8ba734379db101d
Simple Saviour <aryanchoudhary047@gmail.com>
2021-12-24 09:09:13 +0530

    changes

--- scripts/news.js
+++ scripts/news.js
@@ -1,42 +1,4 @@
 
-var cors_api_url = 'https://cors-anywhere.herokuapp.com/';
-function doCORSRequest(options, printResult) {
-  var x = new XMLHttpRequest();
-  x.open(options.method, cors_api_url + options.url);
-  x.onload = x.onerror = function() {
-    printResult(
-      options.method + ' ' + options.url + '\n' +
-      x.status + ' ' + x.statusText + '\n\n' +
-      (x.responseText || '')
-    );
-  };
-  if (/^POST/i.test(options.method)) {
-    x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  }
-  x.send(options.data);
-}
-
-// Bind event
-(function() {
-  var urlField = document.getElementById('url');
-  var dataField = document.getElementById('data');
-  var outputField = document.getElementById('output');
-  document.getElementById('get').onclick =
-  document.getElementById('post').onclick = function(e) {
-    e.preventDefault();
-    doCORSRequest({
-      method: this.id === 'post' ? 'POST' : 'GET',
-      url: urlField.value,
-      data: dataField.value
-    }, function printResult(result) {
-      outputField.value = result;
-    });
-  };
-})();
-if (typeof console === 'object') {
-  console.log('// To test a local CORS Anywhere server, set cors_api_url. For example:');
-  console.log('cors_api_url = "http://localhost:8080/"');
-}
 getnews();
 function getnews(){
 
@@ -48,6 +10,12 @@ if(keyword == ''){
 }
 
 var url = "https://newsapi.org/v2/everything?q="+keyword+"&apiKey=63612b6a99b04113b267076acd03a06d";
+
+jQuery.ajaxPrefilter(function(options) {
+  if (options.crossDomain && jQuery.support.cors) {
+      options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+  }
+});
 
 $.get (url,(response)=>{
     console.log(response.articles);
