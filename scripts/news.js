958ed00bb859e139d85fe89aad2303ac26da9e0e
Simple Saviour <aryanchoudhary047@gmail.com>
2021-12-24 10:34:48 +0530

    new news api added

--- scripts/news.js
+++ scripts/news.js
@@ -9,23 +9,23 @@ if(keyword == ''){
     keyword = "sharemarket";
 }
 
-var url = "https://newsapi.org/v2/everything?q="+keyword+"&apiKey=63612b6a99b04113b267076acd03a06d";
+var url = "https://newsdata.io/api/1/news?apikey=pub_304807a11a986a20ea166c10a4b89bea9d7a&language=en&q=" + keyword;
 
 $.get (url,(response)=>{
-    console.log(response.articles);
+    console.log(response.results);
 
     for(i=1 ; i<16;i= i+3){
         var html = `<div class="card mb-3 shadow">
             <div class="row g-0">
             <div class="col-md-4">
-              <img src="${response.articles[i].urlToImage}" class="img-fluid rounded-start" alt="...">
+              <img src="${response.results[i].image_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">${response.articles[i].title}</h5>
-              <p class="card-text">${response.articles[i].content}</p>
-              <p class="card-text"><small class="text-muted">${response.articles[i].publishedAt} | ${response.articles[i].source.name} | ${response.articles[i].author}</small></p>
-              <a href="${response.articles[i].url}" class="btn btn-primary">Explore</a>
+              <h5 class="card-title">${response.results[i].title}</h5>
+              <p class="card-text">${response.results[i].description}</p>
+              <p class="card-text"><small class="text-muted">${response.results[i].pubDate} | ${response.results[i].source_id} | ${response.results[i].creator}</small></p>
+              <a href="${response.results[i].link}" class="btn btn-primary">Explore</a>
             </div>
             </div>
             </div>
