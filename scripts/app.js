e25790c6f18e7d6d8808750fe521a6059969b83e
Simple Saviour <aryanchoudhary047@gmail.com>
2021-12-31 19:23:01 +0530

    added stock data

---
+++ scripts/app.js
@@ -0,0 +1,262 @@
+getdata();
+function getdata(){
+// var keyword = $("#keywrd").val();
+
+// if(keyword == ''){
+//     keyword = "bitcoin";
+// }
+// var url = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+keyword+"&apikey=5UURJ27EF3L9EKR0";
+// var symbol;
+// $.get (url,(data)=>{
+//     console.log(data);
+//     symbol = data.bestMatches[0]["1. symbol"];
+//     console.log(symbol);
+// });
+
+// var url2 = "http://api.marketstack.com/v1/intraday?access_key=813beab3fd547c787c08ff333640e7c5&symbolS=AAPL&interval=1min";
+
+// $.get (url2,(data)=>{
+//     console.log(data);
+    
+// });
+}
+
+getGainers();
+function getGainers(){
+var url2 = "https://financialmodelingprep.com/api/v3/gainers?apikey=00c78b98526925c7460fcd9725012f2d";
+
+$.get (url2,(data)=>{
+    console.log(data);
+    var html2 = `<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">1.${data[0].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[0].changesPercentage}<br>
+        Current Price : $${data[0].price}<br>
+        Change in Price : $${data[0].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">2.${data[1].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[1].changesPercentage}<br>
+        Current Price : $${data[1].price}<br>
+        Change in Price : $${data[1].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">3.${data[2].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[2].changesPercentage}<br>
+        Current Price : $${data[2].price}<br>
+        Change in Price : $${data[2].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">4.${data[3].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[3].changesPercentage}<br>
+        Current Price : $${data[3].price}<br>
+        Change in Price : $${data[3].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">5.${data[4].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[4].changesPercentage}<br>
+        Current Price : $${data[4].price}<br>
+        Change in Price : $${data[4].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">6.${data[5].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[5].changesPercentage}<br>
+        Current Price : $${data[5].price}<br>
+        Change in Price : $${data[5].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  </div>`;
+$(".getGainers").append(html2);
+});
+}
+
+getLosers();
+function getLosers(){
+var url2 = "https://financialmodelingprep.com/api/v3/losers?apikey=00c78b98526925c7460fcd9725012f2d";
+
+$.get (url2,(data)=>{
+    console.log(data);
+    var html2 = `<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">1.${data[0].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[0].changesPercentage}<br>
+        Current Price : $${data[0].price}<br>
+        Change in Price : $${data[0].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">2.${data[1].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[1].changesPercentage}<br>
+        Current Price : $${data[1].price}<br>
+        Change in Price : $${data[1].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">3.${data[2].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[2].changesPercentage}<br>
+        Current Price : $${data[2].price}<br>
+        Change in Price : $${data[2].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">4.${data[3].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[3].changesPercentage}<br>
+        Current Price : $${data[3].price}<br>
+        Change in Price : $${data[3].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">5.${data[4].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[4].changesPercentage}<br>
+        Current Price : $${data[4].price}<br>
+        Change in Price : $${data[4].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">6.${data[5].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[5].changesPercentage}<br>
+        Current Price : $${data[5].price}<br>
+        Change in Price : $${data[5].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  </div>`;
+$(".getActives").append(html2);
+});
+}
+
+getActives();
+function getActives(){
+var url2 = "https://financialmodelingprep.com/api/v3/actives?apikey=00c78b98526925c7460fcd9725012f2d";
+
+$.get (url2,(data)=>{
+    console.log(data);
+    var html2 = `<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">1.${data[0].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[0].changesPercentage}<br>
+        Current Price : $${data[0].price}<br>
+        Change in Price : $${data[0].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">2.${data[1].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[1].changesPercentage}<br>
+        Current Price : $${data[1].price}<br>
+        Change in Price : $${data[1].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">3.${data[2].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[2].changesPercentage}<br>
+        Current Price : $${data[2].price}<br>
+        Change in Price : $${data[2].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">4.${data[3].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[3].changesPercentage}<br>
+        Current Price : $${data[3].price}<br>
+        Change in Price : $${data[3].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">5.${data[4].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[4].changesPercentage}<br>
+        Current Price : $${data[4].price}<br>
+        Change in Price : $${data[4].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">6.${data[5].companyName}</h5>
+        <p class="card-text">Percentage Change : ${data[5].changesPercentage}<br>
+        Current Price : $${data[5].price}<br>
+        Change in Price : $${data[5].changes}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  </div>`;
+$(".getLosers").append(html2);
+});
+}
